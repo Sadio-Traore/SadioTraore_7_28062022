8227e3a0f3e620500e142425f60c56f754664234
Sadio Traoré <sadiotraore1@gmail.com>
2022-07-12 16:04:29 +0200

    sytème de like/dislike

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,14 +2,20 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 const ObjectID = require('mongoose').Types.ObjectId;
 
+
+//CRUD post
+
 exports.createPost = async (req, res,) => {
     const newPost = new Post({
         userId: req.body.userId,
     message: req.body.message,
     //picture: req.file !== null ? "./images" + fileName : "",
     video: req.body.video,
-    likers: [],
     comments: [],
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [' '],
+    usersdisLiked: [' ']
     })
 
     try {
@@ -22,7 +28,6 @@ exports.createPost = async (req, res,) => {
     
  };
 
-
 exports.readPost = (req, res,next) => {
     Post.find((err,docs) => {
         if(!err) res.send(docs);
@@ -30,8 +35,7 @@ exports.readPost = (req, res,next) => {
     }).sort({ createdAt: -1 });
 
   };
-        
-    
+           
 exports.updatePost = (req, res,next) => {
     if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("Id non reconnu");
@@ -51,6 +55,7 @@ exports.updatePost = (req, res,next) => {
   );
 
 }
+
 exports.deletePost = (req, res,next) => {
     if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unknown : " + req.params.id);
@@ -61,3 +66,76 @@ exports.deletePost = (req, res,next) => {
   });
 
 }
+
+ // Système de like/ dislike
+
+ exports.likePost = (req, res, next) => {
+    let like = req.body.like
+    let userId = req.body.userId
+    let postId = req.params.id
+    
+
+    switch (like) {
+        case 1 :
+            Post.updateOne(
+                { _id: postId }, 
+                { 
+                    $push: { usersLiked: userId },
+                    $inc: { likes: +1 }
+                }
+                 )
+            .then(() => res.status(200).json({ message: "J'aime" }))
+            .catch((error) => res.status(404).json({ error }))
+        
+        break;
+  
+        
+        case -1 :
+            Post.updateOne(
+
+                { _id: postId }, 
+                { 
+                    $inc: { dislikes: +1 },
+                    $push: { usersDisliked: userId }
+                }
+                 )
+            .then(() => { res.status(200).json({ message: "Je n'aime pas" }) })
+            .catch((error) => res.status(404).json({error }))
+            
+            
+            break;
+            
+            case 0 :
+                Post.findOne({ _id: postId })
+                   .then((post) => {
+                    if (post.usersLiked.includes(userId)) { 
+                      Post.updateOne(
+
+                        { _id: postId }, 
+                        { 
+                              $inc: { likes: -1 },
+                              $pull: { usersLiked: userId } 
+                        }
+                            )
+                        .then(() => res.status(200).json({ message: "Neutre" }))
+                        .catch((error) => res.status(404).json({ error }))
+                    }
+                    if (post.usersDisliked.includes(userId)) { 
+                      Post.updateOne(
+                        { _id: postId }, 
+                        { 
+                              $inc: { dislikes: -1 },
+                              $pull: { usersDisliked: userId }
+                        }
+                            )
+                        .then(() => res.status(200).json({ message: "Neutre" }))
+                        .catch((error) => res.status(404).json({ error }))
+                    }
+                  })
+                  .catch((error) => res.status(404).json({ error }))
+              break;
+
+            default:
+                console.log(error);
+            }
+} 
